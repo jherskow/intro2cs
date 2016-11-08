7d37d885d775aaacbee50118626dfd40a19cb710
Josh Herskowitz <jjherskow@gmail.com>
2016-11-08 13:19:32 +0200

    added todo

--- intro2cs/ex1/hello_turtle.py
+++ intro2cs/ex1/hello_turtle.py
@@ -7,9 +7,11 @@
 ##########################################################################
 import turtle
 
+
 def intro_test():
     """This is only a test method for printing hello"""
-    print ("hello")
+    print("hello")
+
 
 def draw_petal():
     """draws one petal of a flower, returning to starting point"""
@@ -21,6 +23,7 @@ def draw_petal():
     turtle.left(45)
     turtle.forward(30)
     turtle.left(135)
+
 
 def draw_flower():
     """draws a flower by rotating the turtle between petals and then
@@ -36,6 +39,7 @@ def draw_flower():
     turtle.right(135)
     turtle.forward(150)
 
+
 def draw_flower_advanced():
     """draws a flower, and then moves turtle from the bottom of the steam
                     to a point 150 to the right of the flower's centre"""
@@ -47,6 +51,7 @@ def draw_flower_advanced():
     turtle.forward(150)
     turtle.right(90)
     turtle.down()
+
 
 def draw_flower_bed():
     turtle.up()
