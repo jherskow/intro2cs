70cc870877cc548fbf5c925d4fe12d235d8684f9
Josh Herskowitz <jjherskow@gmail.com>
2016-11-15 12:47:21 +0200

    fixed  line overflow

--- intro2cs/ex1/math_print.py
+++ intro2cs/ex1/math_print.py
@@ -28,5 +28,6 @@ def e():
 
 def squares_area():
     """prints float value of the squares of 1 through ten"""
-    print(math.pow(1, 2), math.pow(2, 2), math.pow(3, 2), math.pow(4, 2), math.pow(5, 2), math.pow(6, 2),
-          math.pow(7, 2), math.pow(8, 2), math.pow(9, 2), math.pow(10, 2))
+    print(math.pow(1, 2), math.pow(2, 2), math.pow(3, 2), math.pow(4, 2),
+            math.pow(5, 2), math.pow(6, 2),math.pow(7, 2),
+                math.pow(8, 2),math.pow(9, 2), math.pow(10, 2))
