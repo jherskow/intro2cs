8d1af9f81d9013fb205bd3e9ec606897a83554c4
Josh Herskowitz <jjherskow@gmail.com>
2016-11-16 14:38:35 +0200

    more work on user input quadratic

--- intro2cs/ex2/quadratic_equation.py
+++ intro2cs/ex2/quadratic_equation.py
@@ -2,7 +2,7 @@
 # FILE : quadratic_equation.py
 # WRITER : Josuha Herskowitz , jherskow , 321658379
 # EXERCISE : intro2cs ex2 2016-2017
-# DESCRIPTION: A simple quadratic solver.
+# DESCRIPTION: A few quadratic solvers.
 ##########################################################################
 
 def quadratic_equation(a,b,c):
@@ -24,4 +24,22 @@ def quadratic_equation(a,b,c):
     #if discriminant is negative, x will be empty.
     return x
 
-print(quadratic_equation(1,-2,34.5))
+def quadratic_equation_user_input():
+    """
+    Returns list all x solutions for ax^2 +bx +c. = 0
+    a,b,c are all given as inputs.
+    """
+
+    coefficients = input("Insert coefficients a, b, and c: ")
+    parsed = coefficients.split(' ')  # parses coefficients to list
+    a = parsed[0]
+    b = parsed[1]
+    c = parsed[2]
+    x = quadratic_equation(a,b,c)
+    solutions = 0
+    if x[1] != None:
+        print("The equation has 2 solutions: " + x[0] + "and" + x[1])
+    elif x[0] != None
+        print("The equation has 1 solution: " + x[0])
+    else:
+        print("The equation has no solutions")
