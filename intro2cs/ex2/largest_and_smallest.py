c8849f61a8eebff0104743b4546fa8de7b55e799
Josh Herskowitz <jjherskow@gmail.com>
2016-11-17 12:47:50 +0200

    started largest or smallest

---
+++ intro2cs/ex2/largest_and_smallest.py
@@ -0,0 +1,7 @@
+##########################################################################
+# FILE : largest_and_smallest.py
+# WRITER : Josuha Herskowitz , jherskow , 321658379
+# EXERCISE : intro2cs ex2 2016-2017
+# DESCRIPTION: Returns the largest and smallest of 3 numbers.
+# It's easier than trying to find a ruler.
+##########################################################################
