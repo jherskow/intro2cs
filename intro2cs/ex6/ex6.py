7c12caf7ae1233a6ec8bf0d3657e0e96e5dddd49
Josh Herskowitz <jjherskow@gmail.com>
2016-12-13 18:40:40 +0200

    ex6

---
+++ intro2cs/ex6/ex6.py
@@ -0,0 +1,37 @@
+import math as m
+
+
+def compare_pixel(pixel1, pixel2):
+    pass
+
+
+def compare(image1, image2):
+    pass
+
+
+def get_piece(image, upper_left, size):
+    pass
+
+
+def set_piece(image, upper_left, piece):
+    pass
+
+
+def average(image):
+    pass
+
+
+def preprocess_tiles(tiles):
+    pass
+
+
+def get_best_tiles(objective, tiles, averages , num_candidates):
+    pass
+
+
+def choose_tile(piece, tiles):
+    pass
+
+
+def make_mosaic(image, tiles, num_candidates):
+    pass
