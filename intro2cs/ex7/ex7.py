9a6627385961bd8df842a389e5682857112dfa66
Joshua Herskowitz <jjherskow@gmail.com>
2016-12-22 12:30:12 +0200

    created ex7

---
+++ intro2cs/ex7/ex7.py
@@ -0,0 +1,29 @@
+###########################################################################
+# FILE : ex7.py
+# WRITER : Joshua Herskowitz , jherskow , 321658379
+# EXERCISE : intro2cs ex7 2016-2017
+# DESCRIPTION: C
+###########################################################################
+
+# ============ LINEAR RECURSION ===========================================
+
+def print_to_n(n):
+    pass
+
+
+def print_reversed(n):
+    pass
+
+
+def is_prime(n):
+    pass
+
+
+def divisors(n):
+    pass
+
+def exp_n_x(n, x):
+    pass
+
+# ============ NON-LINEAR RECURSION =======================================
+
