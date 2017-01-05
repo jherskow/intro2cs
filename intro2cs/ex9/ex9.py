84c9b33ce68dd67f8cc23c6a70332d08fcca84fb
Joshua Herskowitz <jjherskow@gmail.com>
2017-01-05 16:56:27 +0200

    Added ex9

---
+++ intro2cs/ex9/ex9.py
@@ -0,0 +1,82 @@
+"""########################################################################
+# FILE : ex9.py
+# WRITER : Joshua Herskowitz , jherskow , 321658379
+# WRITER : Rachel Zilberberg, rachelz , 314421876
+# EXERCISE : intro2cs ex9 2016-2017
+# DESCRIPTION: Functions making use of recursive logic.
+#######################################################################"""
+
+
+# ============ Main Class ===========================================
+class Space_Object:
+    """
+    A class representing a n object on the borard
+    """
+
+    # ===== Space_objects - class constants =======
+
+    # ===== Space_objects - class  methods =========
+    def move(self)
+        """
+        moves object position coordinates
+        """
+        pass
+
+    class Ship:
+        """
+        A ship
+        """
+
+    # ===== Ship - class constants =====
+    DEFAULT_SHIP_LIVES = 3
+
+    # ===== Ship - class methods =======
+
+
+def __init__(self, pos, speed, heading):
+    """
+    Initialize a new ship.
+    """
+    self._pos = pos
+    self._heading = heading
+    self._health = DEFAULT_SHIP_LIVES
+    self._
+    self._
+    self._
+
+
+class Asteroid:
+    """
+    An asteroid
+    """
+
+
+def __init__(self, pos, speed, heading, size):
+    """
+    Initialize a new asteroid
+    """
+
+
+self._size = size
+self._pos = pos
+self._heading = heading
+
+
+class Torpedo:
+    """
+    A class representing a n object on the board
+    """
+
+
+def __init__(self, pos, speed, heading):
+    """
+    Initialize a new torpedo
+    """
+    self._size = size
+    self._pos = pos
+    self._heading = heading
+    self._duration
+    self._
+
+
+def redu
