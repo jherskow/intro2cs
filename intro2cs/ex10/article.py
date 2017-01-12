80ed5b006f867be0577b0a7bd18c5b590bc5fc27
Joshua Herskowitz <jjherskow@gmail.com>
2017-01-12 12:41:45 +0200

    setup ex10

---
+++ intro2cs/ex10/article.py
@@ -0,0 +1,24 @@
+"""########################################################################
+# FILE : article.py
+# WRITER : Joshua Herskowitz , jherskow , 321658379
+# EXERCISE : intro2cs ex10 2016-2017
+# DESCRIPTION: ----
+########################################################################"""
+# ========== IMPORTS ======================================================
+
+
+# ========== CLASS ARTICLE ================================================
+
+
+class Article:
+    """
+    A class representing ---.
+    """
+    # ===== Article - class constants =====
+
+    # ===== Article - class methods =======
+
+    def __init__(self):
+        """
+        Initialize a new Article.
+        """
---
+++ intro2cs/ex10/wikinetwork.py
@@ -0,0 +1,24 @@
+"""########################################################################
+# FILE : wikinetwork.py
+# WRITER : Joshua Herskowitz , jherskow , 321658379
+# EXERCISE : intro2cs ex10 2016-2017
+# DESCRIPTION: ----
+########################################################################"""
+# ========== IMPORTS ======================================================
+
+
+# ========== CLASS WIKINETWORK ============================================
+
+
+class WikiNetwork:
+    """
+    A class representing ---.
+    """
+    # ===== WikiNetwork - class constants =====
+
+    # ===== WikiNetwork - class methods =======
+
+    def __init__(self):
+        """
+        Initialize a new WikiNetwork.
+        """
